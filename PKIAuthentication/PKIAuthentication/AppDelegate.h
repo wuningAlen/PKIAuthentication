899d0b7c832a30707db4fe93f781fadb120320a8
alen <wuning1990@163.com>
2018-05-16 22:09:08 +0800

    no message

---
+++ PKIAuthentication/PKIAuthentication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PKIAuthentication
+//
+//  Created by ningwu on 2018/5/16.
+//  Copyright © 2018年 alen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
